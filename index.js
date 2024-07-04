f959a7a3062a6a51afbd1234fc4a1d07a2e46245
Daniel <danielbrodowski97@gmail.com>
2024-07-04 17:52:20 +0100

    make first get requst to riot api work

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import config from './public/config.js';
+import axios from 'axios';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const port = 3000;
 const app = new express();
+const RIOT_API_KEY = config.RIOT_API_KEY;
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,6 +15,22 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
+});
+
+app.get('/account', async (req, res) => {
+  res.render('getAccount.ejs');
+});
+
+app.post('/queryAccount', async (req, res) => {
+  const gameName = req.body.gameName;
+  const tagLine = req.body.tagLine;
+
+  const account = await axios.get(
+    `https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}?api_key=${RIOT_API_KEY}`
+  );
+  const accPuuid = account.data.puuid;
+
+  console.log(accPuuid);
 });
 
 app.listen(port, () => {
