4f68f944542f94cfee8df9616fb2a958535e2180
Daniel <danielbrodowski97@gmail.com>
2024-07-09 11:15:17 +0100

    refactor function retriving player PUUID, abstract some of the code

--- index.js
+++ index.js
@@ -17,17 +17,33 @@ app.get('/', (req, res) => {
   res.render('index.ejs');
 });
 
+//Retrive user PUUID based on username and hashtag
+//Use PUUID to get a list of last 20 played games
 app.post('/queryAccount', async (req, res) => {
+  const userPUUID = await getPUUID(req, res);
+
+  console.log(userPUUID);
+});
+
+//Retrive user PUUID
+const getPUUID = async function (req, res) {
   const gameName = req.body.gameName;
   const tagLine = req.body.tagLine;
 
-  const account = await axios.get(
-    `https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}?api_key=${RIOT_API_KEY}`
-  );
-  const accPuuid = account.data.puuid;
-
-  console.log(accPuuid);
-});
+  try {
+    const account = await axios.get(
+      `https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}?api_key=${RIOT_API_KEY}`
+    );
+    const accPuuid = account.data.puuid;
+    return accPuuid;
+    //res.render('index.ejs', { account: accPuuid });
+  } catch (error) {
+    console.log(error.message);
+    res.render('index.ejs', {
+      error: `Could find a player with name: ${gameName}.`,
+    });
+  }
+};
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
---
+++ notes.js
@@ -0,0 +1,2 @@
+// PROFILE ICONS
+// https://ddragon.leagueoflegends.com/cdn/14.13.1/img/profileicon/685.png
