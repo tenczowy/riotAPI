529e1bee70c707843d2c6ec3d453f5ac8be8bd45
Daniel <danielbrodowski97@gmail.com>
2024-07-10 10:51:21 +0100

    retrive data about last 20 games

--- index.js
+++ index.js
@@ -19,13 +19,30 @@ app.get('/', (req, res) => {
 
 //Retrive user PUUID based on username and hashtag
 //Use PUUID to get a list of last 20 played games
+//////////////////////////////////////////////////
 app.post('/queryAccount', async (req, res) => {
   const userPUUID = await getPUUID(req, res);
+  const matchList = await getMatchList(userPUUID);
 
-  console.log(userPUUID);
+  const matchDataList = [];
+  for (let i = 0; i < matchList.length; i++) {
+    const data = await getMatchData(matchList[i]);
+
+    // console.log(data.info.participants.length);
+    for (let i = 0; i < 10; i++) {
+      if (data.info.participants[i].puuid == userPUUID) {
+        matchDataList.push(data.info.participants[i]);
+      }
+    }
+  }
+
+  console.log(matchDataList[0]);
+  //EUN1_3564431713
+  // console.log(matchDataList[0].;
 });
 
 //Retrive user PUUID
+////////////////////
 const getPUUID = async function (req, res) {
   const gameName = req.body.gameName;
   const tagLine = req.body.tagLine;
@@ -42,6 +59,35 @@ const getPUUID = async function (req, res) {
     res.render('index.ejs', {
       error: `Could find a player with name: ${gameName}.`,
     });
+  }
+};
+
+// Retrive last 20 ranked games ids
+///////////////////////////////////
+const getMatchList = async function (playerPuuid) {
+  try {
+    const result = await axios.get(
+      `https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${playerPuuid}/ids?type=ranked&start=0&count=5&api_key=${RIOT_API_KEY}`
+    );
+    const listOfGames = result.data;
+
+    return listOfGames;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+// Retrive data about games based on game id
+////////////////////////////////////////////
+const getMatchData = async function (gameId) {
+  try {
+    const result = await axios.get(
+      `https://europe.api.riotgames.com/lol/match/v5/matches/${gameId}?api_key=${RIOT_API_KEY}`
+    );
+    const matchData = result.data;
+    return matchData;
+  } catch (error) {
+    console.log(error.message);
   }
 };
 
